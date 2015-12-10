27b16db9414e08a5247035df9dd8a73f722854a5
Aditya Ambadkar <adambadk@cisco.com>
2015-12-09 22:58:00 -0500

    removed some commented code, made servername generic for autoscaling, added chkconfig for service nginx

--- config_files/bootstrap_script.sh
+++ config_files/bootstrap_script.sh
@@ -7,3 +7,6 @@ mv nginx.conf nginx.conf.bak
 aws s3 cp s3://oudtestbucket/nginx.conf .
 mkdir sites-enabled
 aws s3 cp s3://oudtestbucket/oud_nginx.conf ./sites-enabled/oud_nginx.conf
+
+service nginx start
+chkconfig nginx on
